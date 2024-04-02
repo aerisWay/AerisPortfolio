36adf69a8adfacdb7964de04454b253e55ecdd36
aerisWay <al394545@uji.es>
2024-04-02 14:40:06 +0200

    Va bien

--- javascript/slides.js
+++ javascript/slides.js
@@ -27,11 +27,18 @@
 //   dots[slideIndex-1].className += " active";
 // }
 
-let slideIndex = [1,1];
+let slideIndex = [1,1,1,1,1,1,1,1,1];
 /* Class the members of each slideshow group with different CSS classes */
-let slideId = ["mySlides1", "mySlides2"]
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4","mySlides5", "mySlides6","mySlides7", "mySlides8","mySlides9"]
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
+showSlides(1, 6);
+showSlides(1, 7);
+showSlides(1, 8);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
