dc64ed8adb4b406d71161b88bc8ee59d9bcf26b4
aerisWay <al394545@uji.es>
2024-09-27 13:47:30 +0200

    FinalWeb

--- javascript/popup.js
+++ javascript/popup.js
@@ -3,56 +3,56 @@ const projectsData = { //Behind You
       mainGif: "https://www.youtube.com/embed/T-_3d2aRCAo?si=E0yedPrl1r72h1J3vq=hd1080",
       gallery: ['./assets/BehindU/BehindUGameplayFinal.jpg', './assets/BehindU/BehindUGameplayFinal (4).jpg', './assets/BehindU/BehindUGameplayFinal (8).jpg'],
       button: ['./assets/itchio.png', 'https://aerisway.itch.io/behind-you'],
-      description: "My last project, developed in Unreal Engine 5. I used a photorealistic and detailed style. <br> <br> I used a photorealistic and detailed style (Metahuman for the main character) to create a thriller game with hand-crafted levels. The game is a third-person shooter set in an escape room environment. <br> <br> I developed AI agents that communicate with each other in-game, where smaller enemies scream to alert the larger one. <br> <br> I have dedicated a significant amount of time to this personal project and am proud of its polished and professional appearance."
+      description: "My latest project, developed in Unreal Engine 5, features a photorealistic and high-quality style (using Metahuman for the main character) to create a thriller game with hand-crafted levels. The game is a third-person shooter set in an escape room environment. <br> <br> I developed AI agents that communicate with each other in-game, where smaller enemies scream to alert the larger one. <br> <br> I have dedicated a significant amount of time to this personal project and am proud of its polished and professional appearance. <br> <br> I worked alone on this game. I handled all the programming, created the levels using modular assets, crafted the VFX with Niagara, and designed both the gameplay and the theme."
 
     },
     project2: { //RailRoad
-      mainGif: "https://www.youtube.com/embed/T-_3d2aRCAo?si=E0yedPrl1r72h1J3vq=hd1080",
-      gallery: ['./assets/BehindU/BehindUGameplayFinal.jpg', './assets/BehindU/BehindUGameplayFinal (4).jpg', './assets/BehindU/BehindUGameplayFinal (8).jpg'],
-      button: ['./assets/itchio.png', 'https://aerisway.itch.io/behind-you'],
-      description: "My last project, developed in Unreal Engine 5. I used a photorealistic and detailed style. <br> <br> I used a photorealistic and detailed style (Metahuman for the main character) to create a thriller game with hand-crafted levels. The game is a third-person shooter set in an escape room environment. <br> <br> I developed AI agents that communicate with each other in-game, where smaller enemies scream to alert the larger one. <br> <br> I have dedicated a significant amount of time to this personal project and am proud of its polished and professional appearance."
+      mainGif: "",
+      gallery: ['./assets/RailRoad/Estatua.png', './assets/RailRoad/prototipo.png', './assets/RailRoad/pickaxe.png'],
+      button: ['', ''],
+      description: "'Railroad Project' was my internship game, where I was tasked with developing a VR experience that could be played while walking on a treadmill. My idea was to create an experience similar to the famous fair attraction, 'Witch Train.' In this game, the player drives a mine wagon, collecting crystals in a cave while defending against bats and avoiding the cave's traps. <br> <br> Initially, I designed the game to use both hands: one holding a pickaxe to collect crystals and the other wielding a shotgun to fend off dangers. However, through playtesting, I discovered that using both hands while walking blindly on the treadmill caused dizziness. To solve this, I combined the pickaxe and shotgun into a single weapon, allowing the entire game to be controlled with one hand. <br> <br> This internship taught me a lot about VR development in Unity and developing my own shaders. "
     },
 
     project3: { //Segorbe
       mainGif: "https://www.youtube.com/embed/fvu1kAoc4-g?si=8RN5f-vWdQPM3r5Kvq=hd1080",
       gallery: ['./assets/Segorbe/ajedrez.png', './assets/Segorbe/marialuna.jpg', './assets/Segorbe/titulo.jpg'],
-      button: ['./assets/itchio.png', 'https://play.google.com/store/apps/details?id=com.Gamers.Guardianes&hl=es_UY'],
-      description: "My last project, developed in Unreal Engine 5. I used a photorealistic and detailed style. <br> <br> I used a photorealistic and detailed style (Metahuman for the main character) to create a thriller game with hand-crafted levels. The game is a third-person shooter set in an escape room environment. <br> <br> I developed AI agents that communicate with each other in-game, where smaller enemies scream to alert the larger one. <br> <br> I have dedicated a significant amount of time to this personal project and am proud of its polished and professional appearance."
+      button: ['./assets/googlePlayLogo.png', 'https://play.google.com/store/apps/details?id=com.Gamers.Guardianes&hl=es_UY'],
+      description: "'Guardianes de Segorbe' is my first professional project with a company, and it has been published on the Google Play Store. The goal was to create an AR experience to teach users more about the history of a town called Segorbe, in Castellón. The game consists of different levels where the player helps notable figures who were born in or lived in Segorbe at various points in time. <br> <br> The markers used in the game are typically important landmarks in the town, where the characters then appear. It is designed as a route that guides players through the historical center, visiting each significant monument. I was responsible for programming the AR mechanics in Unreal Engine 5 and developing some of the minigames. <br> <br> I learned a lot about Unreal Engine 5 and AR development through this project."
     },
 
     project4: { //Voodoo Wars
-      mainGif: "https://www.youtube.com/embed/T-_3d2aRCAo?si=E0yedPrl1r72h1J3vq=hd1080",
-      gallery: ['./assets/BehindU/BehindUGameplayFinal.jpg', './assets/BehindU/BehindUGameplayFinal (4).jpg', './assets/BehindU/BehindUGameplayFinal (8).jpg'],
+      mainGif: "",
+      gallery: ['./assets/VoodooWars/shader.png', './assets/VoodooWars/finalResult.png', './assets/VoodooWars/pruebaShader.png'],
       button: ['./assets/itchio.png', 'https://aerisway.itch.io/vodoo-wars'],
-      description: "My last project, developed in Unreal Engine 5. I used a photorealistic and detailed style. <br> <br> I used a photorealistic and detailed style (Metahuman for the main character) to create a thriller game with hand-crafted levels. The game is a third-person shooter set in an escape room environment. <br> <br> I developed AI agents that communicate with each other in-game, where smaller enemies scream to alert the larger one. <br> <br> I have dedicated a significant amount of time to this personal project and am proud of its polished and professional appearance."
+      description: "'Voodoo Wars!' was my final degree project. I wanted to create a multiplayer fighting game in a 3D environment. I didn’t realize how responsive and reactive a fighting game needs to be, which made it a challenging experience. I programmed the mechanics, created the character models, and developed the cartoon shader used by the game to calculate lighting. The characters have numerous animations that are carefully coordinated to ensure they remain in unique states, making interactions smoother. <br> <br> The game was initially demonstrated online using a Unity Server that I set up and paid for to present to the evaluation committee. Currently, it is only playable in local multiplayer, but I learned a lot about multiplayer game development, animation and how Unity works through this project. "
     },
 
     project5: { //Rangi
       mainGif: "https://www.youtube.com/embed/ZColxBxBo80?si=gUYuhfx4VZehYkCevq=hd1080",
-      gallery: ['./assets/BehindU/BehindUGameplayFinal.jpg', './assets/BehindU/BehindUGameplayFinal (4).jpg', './assets/BehindU/BehindUGameplayFinal (8).jpg'],
+      gallery: ['./assets/Rangi/pueblo.png', './assets/Rangi/conversacion.png', './assets/Rangi/bosque.png'],
       button: ['./assets/itchio.png', 'https://mariavllr.itch.io/rangi'],
-      description: "My last project, developed in Unreal Engine 5. I used a photorealistic and detailed style. <br> <br> I used a photorealistic and detailed style (Metahuman for the main character) to create a thriller game with hand-crafted levels. The game is a third-person shooter set in an escape room environment. <br> <br> I developed AI agents that communicate with each other in-game, where smaller enemies scream to alert the larger one. <br> <br> I have dedicated a significant amount of time to this personal project and am proud of its polished and professional appearance."
+      description: "'Rangi' is a graphic adventure game that I developed as part of a relatively large project for my degree. I was one of three programmers on the team of five, and I also contributed some art. The game aims to raise awareness about the issues of town disappearance and depopulation through a fictional story set in a world of floating islands. The main character, Akia, is exiled from the city and initially reluctant to live in a small town, but she quickly becomes attached to it. <br> <br> I was primarily responsible for the spider level (as I really enjoy creating combat mechanics in video games), its 2D art, and various other programming tasks within the game. I learned the basics of Unity and how to work collaboratively on a project using GitHub."
     },
 
     project6: { //Typeforce
-      mainGif: "https://www.youtube.com/embed/T-_3d2aRCAo?si=E0yedPrl1r72h1J3vq=hd1080",
-      gallery: ['./assets/BehindU/BehindUGameplayFinal.jpg', './assets/BehindU/BehindUGameplayFinal (4).jpg', './assets/BehindU/BehindUGameplayFinal (8).jpg'],
+      mainGif: "https://www.youtube.com/embed/-WY2KStV0d8?si=47Dg-9t3-FDJ9CVcvq=hd1080",
+      gallery: ['./assets/Typeforce/image2.png', './assets/Typeforce/image3.png', './assets/Typeforce/image4.png'],
       button: ['./assets/itchio.png', 'https://aerisway.itch.io/typeforce'],
-      description: "My last project, developed in Unreal Engine 5. I used a photorealistic and detailed style. <br> <br> I used a photorealistic and detailed style (Metahuman for the main character) to create a thriller game with hand-crafted levels. The game is a third-person shooter set in an escape room environment. <br> <br> I developed AI agents that communicate with each other in-game, where smaller enemies scream to alert the larger one. <br> <br> I have dedicated a significant amount of time to this personal project and am proud of its polished and professional appearance."
+      description: "'Typeforce' is a JavaScript-powered game I created for a degree subject. It was developed using a library called Phaser, which is designed for creating web games. The game involves defeating enemies by typing the English words that appear above them. There are different types of enemies, each corresponding to various difficulty levels, which progressively increase as you survive more waves. If anything touches the character in the center of the screen, the game is over. <br> <br> I significantly improved my typing skills while making this game, and I encourage you to give it a try!"
     },
 
-    project7: { //Game.exe
-      mainGif: "https://www.youtube.com/embed/T-_3d2aRCAo?si=E0yedPrl1r72h1J3vq=hd1080",
-      gallery: ['./assets/BehindU/BehindUGameplayFinal.jpg', './assets/BehindU/BehindUGameplayFinal (4).jpg', './assets/BehindU/BehindUGameplayFinal (8).jpg'],
+    project7: { //System 33
+      mainGif: "https://www.youtube.com/embed/APvcwAp7G1A?si=-HSOIbxorCZdZsUWvq=hd1080",
+      gallery: ['./assets/System33/image4.png', './assets/System33/image5.png', './assets/System33/image6.png'],
       button: ['./assets/itchio.png', 'https://mariavllr.itch.io/system-33'],
-      description: "My last project, developed in Unreal Engine 5. I used a photorealistic and detailed style. <br> <br> I used a photorealistic and detailed style (Metahuman for the main character) to create a thriller game with hand-crafted levels. The game is a third-person shooter set in an escape room environment. <br> <br> I developed AI agents that communicate with each other in-game, where smaller enemies scream to alert the larger one. <br> <br> I have dedicated a significant amount of time to this personal project and am proud of its polished and professional appearance."
+      description: "System 33 is a game jam project I made in 48 hours, where the theme was 'Everything is a weapon.' In my team of four, we conceptualized the idea of simulating an old PC that is under attack by a process, and the player must use the cursor and various programs to generate files to defend the integrity of the system. <br> <br> I was responsible for the programming, and I'm proud of what we accomplished in such a short time during a relaxed game jam where we managed to get plenty of sleep ^^."
     },
 
     project8: { //Tenebris Cordis
       mainGif: "https://www.youtube.com/embed/cYgbT7C9HoE?si=sFkUjcUj3lGi7Y1Zvq=hd1080",
-      gallery: ['./assets/BehindU/BehindUGameplayFinal.jpg', './assets/BehindU/BehindUGameplayFinal (4).jpg', './assets/BehindU/BehindUGameplayFinal (8).jpg'],
-      button: ['./assets/itchio.png', 'https://aerisway.itch.io/behind-you'],
-      description: "My last project, developed in Unreal Engine 5. I used a photorealistic and detailed style. <br> <br> I used a photorealistic and detailed style (Metahuman for the main character) to create a thriller game with hand-crafted levels. The game is a third-person shooter set in an escape room environment. <br> <br> I developed AI agents that communicate with each other in-game, where smaller enemies scream to alert the larger one. <br> <br> I have dedicated a significant amount of time to this personal project and am proud of its polished and professional appearance."
+      gallery: ['./assets/Tenebris/image10.png', './assets/Tenebris/image11.png', './assets/Tenebris/image12.png'],
+      button: ['', ''],
+      description: "'Tenebris Cordis' marks my first experience in game development, dating back seven years. While in high school, my friends and I were inspired by our love for Hollow Knight, which motivated us to create a game with a similar art style. We initially began development using GameMaker but switched to Unity after a few months. Although we had limited knowledge, we were dedicated to learning and improving our skills through effort. <br> <br> In the project, I served as both the character animator and level designer. Tenebris Cordis gained significant popularity within our high school, generating excitement among peers. However, as time went on, each team member pursued different paths, leaving the game with only a teaser trailer and a dedicated fan base awaiting its release."
     }
     // Add more projects as needed
   };
@@ -66,15 +66,60 @@ const projectsData = { //Behind You
     document.getElementById('galleryImg3').src = project.gallery[2];
 
     document.getElementById('mainButton').href = project.button[1];
-    document.getElementById('mainImageButton').src = project.button[0];
+
+     // Manejar la imagen del botón principal
+     const mainImageButton = document.getElementById('mainImageButton');
+    
+     if (!project.button[0]) {  // Si no hay imagen en project.button[0]
+         mainImageButton.style.display = 'none';  // Ocultar la imagen del botón
+         mainImageButton.src = "";  // Limpiar el src por si tenía algún valor anterior
+     } else {
+         mainImageButton.style.display = 'block';  // Mostrar la imagen del botón
+         mainImageButton.src = project.button[0];  // Actualizar el src con la nueva imagen
+         mainImageButton.style.marginLeft = '40%';
+     }
+
    
     document.getElementById('projectDescription').innerHTML = project.description;
 
-    document.getElementById('mainGif').src = project.mainGif;
-   
+    const mainGifContainer = document.getElementById('videoContainer');
+    let mainGif = document.getElementById('mainGif');
+
+    
+
+    if(project.mainGif == ""){
+
+      if (mainGif) {
+        mainGif.style.display = 'none';
+        mainGif.src = ""; // Limpiar el src para detener cualquier reproducción de video.
+    } 
+  } else {
+    // Mostrar y actualizar el src del iframe
+    if (!mainGif) {
+        // Crear el iframe si no existe y añadirlo al contenedor
+        mainGif = document.createElement('iframe');
+        mainGif.id = 'mainGif';
+        mainGif.width = '1280';
+        mainGif.height = '720';
+        mainGif.title = 'YouTube video player';
+        mainGif.frameBorder = '0';
+        mainGif.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
+        mainGif.referrerPolicy = 'strict-origin-when-cross-origin';
+        mainGif.allowFullscreen = true;
+        mainGifContainer.appendChild(mainGif);
+    }
+    mainGif.style.display = 'block';
+    mainGif.src = project.mainGif;
+    mainGif.style.marginLeft = '22%';
+  }
   
     const popup = document.getElementById('popup');
     const popupBackdrop = document.getElementById('popupBackdrop');
+
+    const popupContent = document.getElementById('popupContent'); // Asegúrate de que el popupContent es el contenedor que se desplaza
+    if (popupContent) {
+        popupContent.scrollTop = 0; // Reiniciar el scroll del contenedor del contenido
+    }
   
     // Show backdrop
     popupBackdrop.style.display = 'block'; 
